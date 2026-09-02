90b0c3d33f0cb8201c9ecbb0b7e5572b18560ee3
ouyong520 <70991166+ouyong520@users.noreply.github.com>
2026-09-02 19:10:45 +0800

    Training Farm R0.3: export address-aware RAM snapshot type

--- training/farm/__init__.py
+++ training/farm/__init__.py
@@ -1,12 +1,19 @@
 """WOF Training Farm single-instance emulator adapter package."""
 
-from .adapter import CoreAction, CoreFrameInput, FarmBackend, TrainingFarmAdapter
+from .adapter import (
+    CoreAction,
+    CoreFrameInput,
+    FarmBackend,
+    RamBlockSnapshot,
+    TrainingFarmAdapter,
+)
 from .stable_retro_backend import StableRetroFbneoBackend
 
 __all__ = [
     "CoreAction",
     "CoreFrameInput",
     "FarmBackend",
+    "RamBlockSnapshot",
     "TrainingFarmAdapter",
     "StableRetroFbneoBackend",
 ]
