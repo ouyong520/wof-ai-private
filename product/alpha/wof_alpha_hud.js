2c004e01ef3cda6c58058f8fe1e5bd7f0a871328
ouyong520 <70991166+ouyong520@users.noreply.github.com>
2026-09-03 15:56:33 +0800

    Alpha: fix HUD vertex attrib call after direct P1 warning wiring

--- product/alpha/wof_alpha_hud.js
+++ product/alpha/wof_alpha_hud.js
@@ -149,7 +149,7 @@ function drawLabelPlan(plan){
     gl.viewport(0,0,W,H);gl.disable(gl.DEPTH_TEST);gl.disable(gl.CULL_FACE);gl.disable(gl.SCISSOR_TEST);gl.enable(gl.BLEND);
     gl.blendFunc(gl.SRC_ALPHA,gl.ONE_MINUS_SRC_ALPHA);gl.colorMask(true,true,true,true);gl.useProgram(prog);
     gl.bindBuffer(gl.ARRAY_BUFFER,buf);gl.bufferData(gl.ARRAY_BUFFER,labelVertices,gl.STREAM_DRAW);gl.enableVertexAttribArray(0);
-    gl.vertexAttribPointer(0,4,gl.FLOAT,false,0,gl.FLOAT,false,0,0);gl.activeTexture(gl.TEXTURE0);gl.bindTexture(gl.TEXTURE_2D,labelTex);gl.uniform1i(uTex,0);
+    gl.vertexAttribPointer(0,4,gl.FLOAT,false,0,0);gl.activeTexture(gl.TEXTURE0);gl.bindTexture(gl.TEXTURE_2D,labelTex);gl.uniform1i(uTex,0);
     bridge.nativeDraw.call(gl,gl.TRIANGLES,0,count*6);labelDrawCount+=count;drawCount++;
   }finally{restoreGL(s);}
 }
